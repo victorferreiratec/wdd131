fc7ae5176a39c0a2925a5d304302e84e93438461
victorferreiratec <ferreira.tec.hugo@gmail.com>
2025-03-29 00:45:16 -0300

    modified:   scripts/getdates.js 	modified:   scripts/temples.js 	modified:   styles/temples.css 	modified:   temples.html

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,3 @@
 let d = new Date();
 document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
-document.querySelector('lastModified').textContent = `Last Modification: ${document.lastModified}`;
+document.querySelector('lastModified').textContent = `LastModification: ${document.lastModified}`;
